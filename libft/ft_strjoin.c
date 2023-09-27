c018fe58342ceec6bcdece4d62844c1c2a7828be
Thais Nishimoto <thaismnishimoto@gmail.com>
2023-09-27 02:15:07 -0300

    strjoin remove free call

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 23:39:08 by tmina-ni          #+#    #+#             */
-/*   Updated: 2023/09/22 15:49:18 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2023/09/26 17:46:03 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,5 @@ char	*ft_strjoin(char *s1, char const *s2)
 	ft_memmove(ptr, s1, s1_len);
 	ft_memmove(&ptr[s1_len], s2, s2_len);
 	ptr[s1_len + s2_len] = '\0';
-	free(s1);
 	return (ptr);
 }
